98498775e12927bf7658d6f5f730d66d565786d9
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-11-16 04:37:13 -0300

    update

--- POO/principal-2.py
+++ POO/principal-2.py
@@ -9,13 +9,21 @@ cor = input("Informe a cor do veiculo: ")
 maxima = int(input("Informe a velocidade maxima do veiculo: "))
 
 car = Automoveis(marca, modelo, cor, maxima)
-
 print(" ========================== RETORNO DE DADOS ============================ ")
 print("A marca do veiculo é: ", car.marca)
 print("O modelo do veiculo é: ", car.modelo)
 print("A cor do veiculo é: ", car.cor)
 print("A velocidade maxima do veiculo é: ", car.veloMax)
+
+print(" ========================== Teste de Aceleração ============================ ")
 car.ligado = True  # Ligando o Chevette...
 for a in range(4):
-    car.acelerar(50)
-print("Velocidade Atual: ", car.veloAtual)  # VeloMax
+    car.acelerar(50) # Acelerando 200km
+print("Acelerando...")
+print("A velocidade atual do veiculo é: ", car.veloAtual)
+
+print(" ========================== Teste de Freagem ============================ ")
+for f in range(4):
+    car.frear(10) # Freando a 40km
+print("Freando...")
+print("A velocidade atual do veiculo é: ", car.veloAtual)
